0961acca32c0163643f46c10a9861eb1cb5bc72c
mr.ahner <tommy.james14@proton.me>
2025-04-21 12:58:38 -0700

    add simple ip list

--- mod/dashboard.php
+++ mod/dashboard.php
@@ -1,1 +1,11 @@
-
+<?php
+// small dashboard
+if (!isset['mod']) {
+  die ("nu nu mod :9");
+} 
+echo "<h2>ip list</h2>";
+$iusersfile = fopen("../ips.txt", "r") or die("unable to list ips");
+echo fread($iusersfile,filesize("../ips.txt"));
+fclose($iusersfile);
+echo "<hr>";
+?>
