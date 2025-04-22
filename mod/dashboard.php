b1b8c4be896c6fbaca02b8bd9458bb564870597f
mr.ahner <tommy.james14@proton.me>
2025-04-22 16:31:37 -0700

    fix session errorr

--- mod/dashboard.php
+++ mod/dashboard.php
@@ -1,13 +1,15 @@
 <?php
+session_start();
 // small dashboard
-if (!isset['mod']) {
-  die ("nu nu mod :9");
-} 
+if (!isset($_SESSION['mod'])) {
+  die("nu nu mod :9");
+}
 echo "<h2>ip list</h2>";
 $iusersfile = fopen("../ips.txt", "r") or die("unable to list ips");
 echo fread($iusersfile,filesize("../ips.txt"));
 fclose($iusersfile);
 echo "<hr>";
+
 
 $mod = "['yourname']"; //tim - you can chan ge this, just to your name         
 
